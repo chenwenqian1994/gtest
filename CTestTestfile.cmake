8463e5e1e1e7b5644fae72498987abc0bdc53b34
chenwenqian1994 <854165535@qq.com>
2018-10-09 14:17:57 +0800

    初始化

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: F:/源码/googletest-master
+# Build directory: F:/源码/gtest_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
